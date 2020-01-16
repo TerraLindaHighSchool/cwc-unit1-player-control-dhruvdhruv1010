003f47bbcaf66b007a66518984e9834db85751f2
dhruvdhruv1010 <59099019+dhruvdhruv1010@users.noreply.github.com>
2020-01-16 15:24:57 -0800

    1.4.4 c

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -14,12 +14,15 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public float turnSpeed;
     public float horizontalInput;
+    public float forwardInput;
+
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         //Move the vehicle forward
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
         
     }
