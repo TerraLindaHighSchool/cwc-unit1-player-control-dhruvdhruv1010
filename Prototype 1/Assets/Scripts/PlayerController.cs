d19ebaf6ca53d02c00935bbde225b20c43b48ca6
dhruvdhruv1010 <59099019+dhruvdhruv1010@users.noreply.github.com>
2020-01-20 11:19:58 -0800

    1.4.6 c

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -11,10 +11,10 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    private float speed = 5.0f;
+    private float turnSpeed = 25;
+    private float horizontalInput;
+    private float forwardInput;
 
     void Update()
     {
@@ -23,7 +23,7 @@ public class PlayerController : MonoBehaviour
         //Move the vehicle forward
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
         
     }
 }
